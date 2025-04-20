f1f3637f321a9e517ed3d5d48806e9c91d758ff9
FluffyVader <wewilllearnthis@gmail.com>
2025-04-20 12:38:27 +0100

    Made the screen black
    Made the screen black and added a white line on it

--- plane.py
+++ plane.py
@@ -9,12 +9,21 @@ from pygame.locals import (
     QUIT,
 )
 
-pygame.init()
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+pygame.init()
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = Player()
 
 running = True
 
@@ -26,19 +35,19 @@ while running:
         elif event.type == QUIT:
             running = False
 
-screen.fill((255, 255, 255))
+    screen.fill((0, 0, 0))
+    screen.blit(player.surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
 
-surf = pygame.Surface((50, 50))
-surf.fill((0, 0, 0))
+    pygame.display.flip()
 
-rect = surf.get_rect()
+pressed_keys = pygame.key.get_pressed()
 
-screen.blit(surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
-pygame.display.flip()
-
-surf_center = (
-    (SCREEN_WIDTH-surf.get_width())/2.0
-    (SCREEN_HEIGHT-surf.get_height())/2.0)
-
-screen.blit(surf, surf_center)
-pygame.display.flip()
+def update(self, pressed_keys):
+    if pressed_keys[K_UP]:
+        self.rect.move_ip(0, -5)
+    if pressed_keys[K_DOWN]:
+        self.rect.move_ip(0, 5)
+    if pressed_keys[K_LEFT]:
+        self.rect.move_ip(-5, 0)
+    if pressed_keys[K_RIGHT]:
+        self.rect.move_ip(5, 0)
