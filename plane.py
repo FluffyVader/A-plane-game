43b4efbbcc6243fcfa528c518cffc61011745837
FluffyVader <wewilllearnthis@gmail.com>
2025-05-02 19:02:31 +0100

    Made the player rectangle move around the screen with the arrow keys

--- plane.py
+++ plane.py
@@ -14,18 +14,69 @@ import random
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+###################################################################################################
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         self.surf = pygame.Surface((75, 25))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
+        #self.rect.x = SCREEN_WIDTH/2 - self.rect.width/2
+        #self.rect.y = SCREEN_HEIGHT/2 - self.rect.height/2
+        self.movement_speed = 1
+
+    
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+           self.rect.move_ip(0, -self.movement_speed)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, self.movement_speed)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-self.movement_speed, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(self.movement_speed, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+###################################################################################################
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.Surface((20, 10))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+        center=(random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),random.randint(0, SCREEN_HEIGHT))
+
+        self.speed = random.randint(5, 20)
+
+###################################################################################################
 
 pygame.init()
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+ADDENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(ADDENEMY, 250)
+
 
 player = Player()
+
+# Create groups to hold enemy sprites and all sprites
+# - enemies is used for collision detection and position updates
+# - all_sprites is used for rendering
+enemies = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
 
 running = True
 
@@ -36,42 +87,35 @@ while running:
                 running = False
         elif event.type == QUIT:
             running = False
-    
-    pressed_keys = pygame.key.get_pressed()
 
-    screen.fill((0, 0, 0))
+        elif event.type == ADDENEMY:
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+
     
-    screen.blit(player.surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
     pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+
+    screen_surface.fill((0, 0, 0))
+    
+    screen_surface.blit(player.surf, player.rect)
+    #screen_surface.blit(player.surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+    
+    
+
+    #enemies.update()
 
     pygame.display.flip()
 
-pressed_keys = pygame.key.get_pressed()
 
-def update(self, pressed_keys):
-    if pressed_keys[K_UP]:
-        self.rect.move_ip(0, -5)
-    if pressed_keys[K_DOWN]:
-        self.rect.move_ip(0, 5)
-    if pressed_keys[K_LEFT]:
-        self.rect.move_ip(-5, 0)
-    if pressed_keys[K_RIGHT]:
-        self.rect.move_ip(5, 0)
 
-    if self.rect.left < 0:
-        self.rect.left = 0
-    if self.rect.right > SCREEN_WIDTH:
-        self.rect.right = SCREEN_WIDTH
-    if self.rect.top <= 0:
-        self.rect.top = 0
-    if self.rect.bottom >= SCREEN_HEIGHT:
-        self.rect.bottom = SCREEN_HEIGHT
 
-class Enemy(pygame.sprite.Sprite):
-    def __init__(self):
-        super(Enemy, self).__init__()
-        self.surf = pygame.Surface((20, 10))
-        self.surf.fill((255, 255, 255))
-        self.rect = self.surf.get_rect()
-        center=(random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),random.randint(0, SCREEN_HEIGHT))
- 
+
+
+    #def update(self):
+    #    self.rect.move_ip(-self.speed, 0)
+    #    if self.rect.right < 0:
+    #        self.kill()
+
+
