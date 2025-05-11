5c3becf619d987ea35f5e762cb051403fb1567cd
FluffyVader <wewilllearnthis@gmail.com>
2025-05-11 19:35:39 +0100

    Now, the game has clouds added in
    Now, the game has clouds added in and the background is light blue so the plane
    looks like its in the air.

--- plane.py
+++ plane.py
@@ -70,8 +70,29 @@ class Enemy(pygame.sprite.Sprite):
 
         self.speed = random.randint(5, 20)
 
+
+
+
+
     def update(self):
         self.rect.move_ip(-self.speed, 0)
+        if self.rect.right < 0:
+            self.kill()
+
+class Cloud(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Cloud, self).__init__()
+        self.surf = pygame.image.load("cloud.png").convert()
+        self.surf.set_colorkey((0, 0, 0), RLEACCEL)
+        # The starting position is randomly generated
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT),
+            )
+        )
+    def update(self):
+        self.rect.move_ip(-5, 0)
         if self.rect.right < 0:
             self.kill()
 ###################################################################################################
@@ -83,6 +104,8 @@ screen_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 # Create a custom event for adding a new enemy
 ADDENEMY = pygame.USEREVENT + 1
 pygame.time.set_timer(ADDENEMY, 1000)
+ADDCLOUD = pygame.USEREVENT + 2
+pygame.time.set_timer(ADDCLOUD, 1000)
 
 
 player = Player()
@@ -92,6 +115,7 @@ player = Player()
 # - enemies is used for collision detection and position updates
 # - all_sprites is used for rendering
 enemies = pygame.sprite.Group()
+clouds = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
@@ -112,6 +136,11 @@ while running:
             enemies.add(new_enemy)
             all_sprites.add(new_enemy)
 
+        elif event.type == ADDCLOUD:
+            new_cloud = Cloud()
+            clouds.add(new_cloud)
+            all_sprites.add(new_cloud)
+
     
     pressed_keys = pygame.key.get_pressed()
     player.update(pressed_keys)
@@ -120,9 +149,10 @@ while running:
     #for enemy in enemies:
     #    enemy.update()
     enemies.update()
+    clouds.update()
 
+    screen_surface.fill((135,206,250))
 
-    screen_surface.fill((0, 0, 0))
     
     for entity in all_sprites:
         screen_surface.blit(entity.surf, entity.rect)
